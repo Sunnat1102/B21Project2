d03e9582d41462a47d6f44550648b557e0594610
Sunnat Davletov <Davletovnyc@gmail.com>
2020-11-10 15:05:40 -0500

    day november10

--- src/Hi.java
+++ src/Hi.java
@@ -1,6 +1,6 @@
 public class Hi {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world");
         // Today is great day i love learning Java
 
         // hello world
